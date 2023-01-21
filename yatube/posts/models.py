824765721b3a6f1e3a58b505b6b2d55dc357f207
antoncp <antoncp@gmail.com>
2023-01-21 18:23:52 +0100

    Improve templates and  models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название сообщества'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='URL-раздел сообщества (ENG)'
+    )
+    description = models.TextField(verbose_name='Описание сообщества')
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Post(models.Model):
+    text = models.TextField(verbose_name='Текст поста')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Сообщество'
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+NUM_POSTS_PER_PAGE = 10
 
 
 def index(request):
-    return HttpResponse('First main page')
+    template = 'posts/index.html'
+    posts = Post.objects.all()[:NUM_POSTS_PER_PAGE]
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'A post, written by {slug}')
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = (Post.objects.filter(group=group)[:NUM_POSTS_PER_PAGE])
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
