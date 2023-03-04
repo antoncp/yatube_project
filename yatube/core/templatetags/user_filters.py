2c6739bc3c0dbb81fdb74c4c14c7194d84daf304
antoncp <antoncp@gmail.com>
2023-03-04 14:41:08 +0100

    Update search to case sensitive

--- yatube/core/templatetags/user_filters.py
+++ yatube/core/templatetags/user_filters.py
@@ -15,9 +15,18 @@ def addclass(field, css):
 def highlight(text, value):
     if text is not None:
         text = str(text)
-        src_str = re.compile(value, re.IGNORECASE)
-        str_replaced = src_str.sub(f"<span class=\"highlight\">{value}</span>", text)
+        src_str = re.compile(f'({value})', re.IGNORECASE)
+        str_replaced = src_str.sub(r"<span class='highlight'>\1</span>", text)
     else:
         str_replaced = ''
+
+    sentences = str_replaced.split('.')
+    for num, sentence in enumerate(sentences):
+        if value.lower() in sentence.lower():
+            if len(sentences) - num > 1:
+                str_replaced = '.'.join(sentences[num:num + 3]) + '...'
+            else:
+                str_replaced = str(sentence) + '...'
+            break
 
     return mark_safe(str_replaced)
